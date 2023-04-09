b7199009e7c777e80e3681e92589cde00ebdfbba
José Lopes <107268852+joselopes04@users.noreply.github.com>
2023-04-09 23:15:19 +0100

    Added the homeScreen

--- src/index.php
+++ src/index.php
@@ -11,10 +11,23 @@
     <script src="main.js"></script>
 </head>
 <?php include 'navbar.html' ?>
+
 <body class="bg-gray-50 dark:bg-gray-800">
+<div class="md:flex md:flex-row ml-4 my-10">
+    <div class="md:w-3/6 flex flex-col justify-center items-center md:ml-12">
+        <h1 class="text-5xl mb-5 dark:text-white">Football Jersey Shop</h1>
+        <p class="mb-3 dark:text-white">From the pitch to the stands, FJS has you covered!</p>
+        <button class="bg-blue-800 rounded-xl w-48 text-white py-2 hover:scale-105 duration-300">Shop Now</button>
+    </div>
+    <div class="mt-5 md:w-3/6 mr-12">
+        <img src="../images/football.svg" alt="Football Fans" class="w-full">
+    </div>
+</div>
+
+
+
   
-    <h1 class="text-3xl dark:text-white">Football Jersey Shop</h1>
-    <p class="dark:text-white">From the pitch to the stands, Football Jersey Shop has you covered</p>
 </body>
-<?php include 'footer.html' ?> 
+<?php include 'footer.html' ?>
+
 </html>
