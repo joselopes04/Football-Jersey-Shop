e98cbc49b01ddf69253fd322ec55db1a3f264e47
José Lopes <107268852+joselopes04@users.noreply.github.com>
2023-04-12 11:10:04 +0100

    Added the design for home page and the design for the general products view

--- src/index.php
+++ src/index.php
@@ -13,20 +13,154 @@
 <?php include 'navbar.html' ?>
 
 <body class="bg-gray-50 dark:bg-gray-800">
-<div class="md:flex md:flex-row ml-4 my-10">
-    <div class="md:w-3/6 flex flex-col justify-center items-center md:ml-12">
-        <h1 class="text-5xl mb-5 dark:text-white">Football Jersey Shop</h1>
-        <p class="mb-3 dark:text-white">From the pitch to the stands, FJS has you covered!</p>
-        <button class="bg-blue-800 rounded-xl w-48 text-white py-2 hover:scale-105 duration-300">Shop Now</button>
+    <div class="md:flex md:flex-row ml-4 my-10">
+        <div class="md:w-3/6 flex flex-col justify-center items-center md:ml-12">
+            <h1 class="text-5xl mb-5 dark:text-white">Football Jersey Shop</h1>
+            <p class="mb-3 dark:text-white">From the pitch to the stands, FJS has you covered!</p>
+            <a href="#popularProducts" class="flex flex-row justify-center px-3 py-2 bg-blue-800 rounded-xl w-48 text-white hover:scale-105 duration-300">Shop Now</a>
+        </div>
+        <div class="mt-5 md:w-3/6 mr-12">
+            <img src="../images/football.svg" alt="Football Fans" class="w-full">
+        </div>
     </div>
-    <div class="mt-5 md:w-3/6 mr-12">
-        <img src="../images/football.svg" alt="Football Fans" class="w-full">
+    <!-- Popular Products -->
+    <div id="popularProducts" class="my-20">
+        <div class="flex flex-row justify-between mx-5">
+            <h2 class="text-3xl dark:text-white">Popular</h2>
+            <a class="p-2 dark:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                </svg>
+                View All</a>
+        </div>
+        <div class="grid grid-flow grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 mx-5">
+            <div class="shadow-lg rounded-lg">
+                <a href="">
+                    <img src="../images/jerseys/benfica2223.png" alt="">
+                </a>
+                <div class="p-5">
+                    <h3 class="text-xl dark:text-white mb-2">Benfica 22/23</h3>
+                    <div class="flex flex-row dark:text-white">
+                        <h6>Price: </h6>
+                        <h6>50€</h6>
+                    </div> 
+                    <div class="flex flex-row my-3">
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">XL</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">L</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">M</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">S</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">XS</div>
+                    </div>
+                    <div class="flex flex-col sm:flex-row justify-between">
+                        <a class="flex flex-row justify-center px-3 py-2 my-2 bg-gradient-to-r from-red-500 to-orange-500 rounded-xl w-36 text-sm text-white hover:scale-105 duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                            </svg>Add to chart
+                        </a>
+                        <a class="flex flex-row justify-center px-3 py-2 my-2 bg-gradient-to-r from-blue-800  to-blue-600 rounded-xl w-36 text-sm text-white hover:scale-105 duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                            </svg>
+                            View details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="shadow-lg rounded-lg">
+                <a href="">
+                    <img src="../images/jerseys/barcelona2223.jpg" alt="">
+                </a>
+                <div class="p-5">
+                    <h3 class="text-xl dark:text-white mb-2">Barcelona 22/23</h3>
+                    <div class="flex flex-row dark:text-white">
+                        <h6>Price: </h6>
+                        <h6>50€</h6>
+                    </div> 
+                    <div class="flex flex-row my-3">
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">XL</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">L</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">M</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">S</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">XS</div>
+                    </div>
+                    <div class="flex flex-col sm:flex-row justify-between">
+                        <a class="flex flex-row justify-center px-3 py-2 my-2 bg-gradient-to-r from-red-500 to-orange-500 rounded-xl w-36 text-sm text-white hover:scale-105 duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                            </svg>Add to chart
+                        </a>
+                        <a class="px-3 py-2 my-2 flex flex-row justify-center bg-gradient-to-r from-blue-800  to-blue-600 rounded-xl w-36 text-sm text-white hover:scale-105 duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                            </svg>
+                            View details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="shadow-lg rounded-lg">
+                <a href="">
+                    <img src="../images/jerseys/juventus2223.jpg" alt="">
+                </a>
+                <div class="p-5">
+                    <h3 class="text-xl dark:text-white mb-2">Juventus 22/23</h3>
+                    <div class="flex flex-row dark:text-white">
+                        <h6>Price: </h6>
+                        <h6>50€</h6>
+                    </div> 
+                    <div class="flex flex-row my-3">
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">XL</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">L</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">M</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">S</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">XS</div>
+                    </div>
+                    <div class="flex flex-col sm:flex-row justify-between">
+                        <a class="flex flex-row justify-center px-3 py-2 my-2 bg-gradient-to-r from-red-500 to-orange-500 rounded-xl w-36 text-sm text-white hover:scale-105 duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                            </svg>Add to chart
+                        </a>
+                        <a class="flex flex-row justify-center px-3 py-2 my-2 bg-gradient-to-r from-blue-800  to-blue-600 rounded-xl w-36 text-sm text-white hover:scale-105 duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                            </svg>
+                            View details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="shadow-lg rounded-lg">
+                <a href="">
+                    <img src="../images/jerseys/arsenal2223.png" alt="">
+                </a>
+                
+                <div class="p-5">
+                    <h3 class="text-xl dark:text-white mb-2">Arsenal 22/23</h3>
+                    <div class="flex flex-row dark:text-white">
+                        <h6>Price: </h6>
+                        <h6>50€</h6>
+                    </div> 
+                    <div class="flex flex-row my-3">
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">XL</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">L</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">M</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">S</div>
+                        <div class="border-2 border-gray-300 text-gray-400 rounded-md text-xs px-2 py-1 mr-2">XS</div>
+                    </div>
+                    <div class="flex flex-col sm:flex-row justify-between">
+                        <a class="flex flex-row justify-center px-3 py-2 my-2 bg-gradient-to-r from-red-500 to-orange-500 rounded-xl w-36 text-sm text-white hover:scale-105 duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                            </svg>Add to chart
+                        </a>
+                        <a class="flex flex-row justify-center px-3 py-2 my-2 bg-gradient-to-r from-blue-800  to-blue-600 rounded-xl w-36 text-sm text-white hover:scale-105 duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                            </svg>
+                            View details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-
-
-
-  
 </body>
 <?php include 'footer.html' ?>
 
